3e31bc04e5b84b29feea63db5110ad5213c05c5f
IldarSaygafarov2 <sayildar17@gmail.com>
2025-05-13 17:44:04 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# django
+# pillow
+
+# django-admin - утилита для запуска команд django
+
+# создание проекта
+# django-admin startproject <project_name> - создаст отдельную папку для проекта
+# django-admin startproject <project_name> . - создаст отдельную папку для проекта
+# 
+# переходим в папку проекта
+# cd project_name
+
+# создание приложения внутри проекта
+# python manage.py startapp app_name
+ 
+# запуск локального сервера
+# хост по умолчанию (127.0.0.1:8000)
+# python manage.py runserver 4561
+# python manage.py runserver 5000
+
+# API - application program interface
+
+
+# TODO:  работа с гитом
+
+
+# инициализация гита в проекта
+# git init - инициализирует пустой гит репозиторий в папке проекта
+# git status - проверяет добавляемые папки\файлы в репозиторий
+# git add <название_файл>\. - добавляет все измененные или новые файлы и папки для просмотра
+
+# .gitignore - файл в котором описывается то, что мы не должны заливать на гитхаб
